71292fd8406f1bbdc3423be1e7905d5ef7f4e934
vinicius <viniciusarashiro0@gmail.com>
2023-04-24 15:39:47 -0300

    Criação do controller, domain e util

---
+++ src/main/java/academy/codelab/springboot2/controller/AnimeController.java
@@ -0,0 +1,26 @@
+package academy.codelab.springboot2.controller;
+
+import academy.codelab.springboot2.domain.Anime;
+import academy.codelab.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+    @GetMapping(path = "list")
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku no hero"), new Anime("Berserk"));
+    }
+}
