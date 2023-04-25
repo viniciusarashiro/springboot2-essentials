3be71447173dfb4551e5cb7e474105c4caa2b371
vinicius <viniciusarashiro0@gmail.com>
2023-04-25 17:54:00 -0300

    Validação de campos com o Spring validation

--- src/main/java/academy/codelab/springboot2/domain/Anime.java
+++ src/main/java/academy/codelab/springboot2/domain/Anime.java
@@ -1,10 +1,7 @@
 package academy.codelab.springboot2.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
