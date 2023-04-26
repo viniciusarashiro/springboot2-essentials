5aaf275aa8e278992e7eeec1a8d44288a06d0f1b
vinicius <viniciusarashiro0@gmail.com>
2023-04-26 19:02:00 -0300

    JPA Test para exceções quando o nome for vazio

--- src/main/java/academy/codelab/springboot2/domain/Anime.java
+++ src/main/java/academy/codelab/springboot2/domain/Anime.java
@@ -2,6 +2,7 @@ package academy.codelab.springboot2.domain;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,5 +17,7 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
