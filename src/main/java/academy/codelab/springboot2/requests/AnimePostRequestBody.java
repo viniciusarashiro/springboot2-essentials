a76c2c210858b79e7ebbe05d83429963bd18ad64
vinicius <viniciusarashiro0@gmail.com>
2023-04-28 15:33:07 -0300

    Testes de Integração

--- src/main/java/academy/codelab/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/codelab/springboot2/requests/AnimePostRequestBody.java
@@ -1,12 +1,15 @@
 package academy.codelab.springboot2.requests;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     private String name;
